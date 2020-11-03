41f0a7edb1edcfc39d2361d8ae17ccf7a3aa8417
autopp <autopp.inc@gmail.com>
2020-11-03 23:24:07 +0900

    fix: disable eslint rule in helper.ts

--- test/helper.ts
+++ test/helper.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export function describeEach<T extends any[]>(
   table: [string, ...T][],

4be93844d7f8763a8ea4df53ea81439899fb327a
autopp <autopp.inc@gmail.com>
2020-11-19 21:52:20 +0900

    test: improve describeEach

--- test/helper.ts
+++ test/helper.ts
@@ -2,14 +2,27 @@
 
 export function describeEach<T extends any[]>(
   table: [string, ...T][],
-  fn: (...args: T) => any,
-  timeout?: number
+  fn: (...args: T) => any
+): void
+
+export function describeEach<T extends any[]>(
+  name: string,
+  table: [...T][],
+  fn: (...args: T) => any
+): void
+
+export function describeEach<T extends any[]>(
+  ...args:
+    | [table: [string, ...T][], fn: (...args: T) => any]
+    | [name: string, table: [...T][], fn: (...args: T) => any]
 ): void {
-  describe.each(table)(
-    "%s",
-    (_name, ...args: T) => {
+  if (args.length === 2) {
+    const [table, fn] = args
+    describe.each(table)("%s", (_name, ...args: T) => {
       fn(...args)
-    },
-    timeout
-  )
+    })
+  } else {
+    const [name, table, fn] = args
+    describe.each(table)(name, fn)
+  }
 }

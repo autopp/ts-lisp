f84b9a372cd298dc571346f1d82d478149bd98ab
autopp <autopp.inc@gmail.com>
2020-11-21 14:58:35 +0900

    refactor: add factory and constants for token

--- src/parser.ts
+++ src/parser.ts
@@ -12,7 +12,21 @@ export type Token = {
   text: string
 }
 
+export const TRUE_TOKEN: Token = { type: "true", text: "#t" }
+export const FALSE_TOKEN: Token = { type: "false", text: "#f" }
+export const LPAREN_TOKEN: Token = { type: "lparen", text: "(" }
+export const RPAREN_TOKEN: Token = { type: "rparen", text: ")" }
+export const QUOTE_TOKEN: Token = { type: "quote", text: "'" }
+export const DOT_TOKEN: Token = { type: "dot", text: "." }
 export const EOI_TOKEN: Token = { type: "eoi", text: "" }
+
+export function makeNumToken(text: string): Token {
+  return { type: "num", text }
+}
+
+export function makeSymToken(text: string): Token {
+  return { type: "sym", text }
+}
 
 export class ParserError extends Error {
   constructor(message: string) {
@@ -22,26 +36,28 @@ export class ParserError extends Error {
 }
 
 export function tokenize(source: string): Token[] {
-  const rules: [RegExp, Token["type"] | undefined][] = [
-    [/^\s+/, undefined],
-    [/^[-+]?\d+/, "num"],
-    [/^#t/, "true"],
-    [/^#f/, "false"],
-    [/^[-+*/_a-zA-Z<=>!?a-zA-Z][-+*/_a-zA-Z<=>!?a-zA-Z0-9]*/, "sym"],
-    [/^\(/, "lparen"],
-    [/^\)/, "rparen"],
-    [/^'/, "quote"],
-    [/^\./, "dot"],
+  const rules: [RegExp, (text: string) => Token | undefined][] = [
+    [/^\s+/, () => undefined],
+    [/^[-+]?\d+/, makeNumToken],
+    [/^#t/, () => TRUE_TOKEN],
+    [/^#f/, () => FALSE_TOKEN],
+    [/^[-+*/_a-zA-Z<=>!?a-zA-Z][-+*/_a-zA-Z<=>!?a-zA-Z0-9]*/, makeSymToken],
+    [/^\(/, () => LPAREN_TOKEN],
+    [/^\)/, () => RPAREN_TOKEN],
+    [/^'/, () => QUOTE_TOKEN],
+    [/^\./, () => DOT_TOKEN],
   ]
 
   const tokens: Token[] = []
   let rest = source
   while (rest !== "") {
-    let matched: { text: string; type: Token["type"] | undefined } | undefined
-    for (const [pattern, type] of rules) {
+    let matched:
+      | { text: string; makeToken: (text: string) => Token | undefined }
+      | undefined
+    for (const [pattern, makeToken] of rules) {
       const regMatched = pattern.exec(rest)
       if (regMatched !== null) {
-        matched = { text: regMatched[0], type }
+        matched = { text: regMatched[0], makeToken }
         break
       }
     }
@@ -49,8 +65,9 @@ export function tokenize(source: string): Token[] {
       throw new ParserError(`unrecognized character found "${rest[0]}"`)
     }
 
-    if (matched.type !== undefined) {
-      tokens.push({ type: matched.type, text: matched.text })
+    const token = matched.makeToken(matched.text)
+    if (token !== undefined) {
+      tokens.push(token)
     }
 
     rest = rest.substring(matched.text.length)

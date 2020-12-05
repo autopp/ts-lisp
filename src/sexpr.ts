f5d9b76f4d6190d306ea9ad320a77ab8e1e19eb8
autopp <autopp.inc@gmail.com>
2020-12-06 03:19:43 +0900

    feat: add procs

--- src/sexpr.ts
+++ src/sexpr.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Opaque } from "ts-essentials"
+import { Env } from "./env"
+import { EvalResult } from "./eval"
 
 export type Nil = Opaque<null, "Nil">
 export const NIL: Nil = null as Nil
@@ -35,19 +37,113 @@ export function isSym(x: any): x is Sym {
 }
 
 export type Cons = {
+  type: "cons"
   car: SExpr
   cdr: SExpr
 }
 export function makeCons(car: SExpr, cdr: SExpr): Cons {
-  return { car, cdr }
+  return { type: "cons", car, cdr }
 }
-export function isCons(x: any): x is Cons {
+export function isCons(x: SExpr): x is Cons {
+  return x && typeof x === "object" && (x as any).type === "cons"
+}
+
+export type Arity = {
+  required: number
+  optional: number
+  hasRest: boolean
+}
+function makeArity(x: Partial<Arity>): Arity {
+  return {
+    required: x.required || 0,
+    optional: x.optional || 0,
+    hasRest: !!x.hasRest,
+  }
+}
+
+type ProcBody = (args: SExpr[], env: Env) => EvalResult
+
+export type SpForm = {
+  type: "spForm"
+  name: string
+  arity: Arity
+  body: ProcBody
+}
+export function makeSpForm(
+  name: string,
+  arity: Partial<Arity>,
+  body: ProcBody
+): SpForm {
+  return { type: "spForm", name, arity: makeArity(arity), body }
+}
+export function isSpForm(x: SExpr): x is SpForm {
+  return x && typeof x === "object" && (x as { type: string }).type === "spForm"
+}
+
+export type BuiltinFunc = {
+  type: "builtin"
+  name: string
+  arity: Arity
+  body: ProcBody
+}
+export function makeBuiltinFunc(
+  name: string,
+  arity: Partial<Arity>,
+  body: ProcBody
+): BuiltinFunc {
+  return { type: "builtin", name, arity: makeArity(arity), body }
+}
+export function isBuiltinFunc(x: SExpr): x is BuiltinFunc {
   return (
-    x && typeof x === "object" && x.car !== undefined && x.cdr !== undefined
+    x && typeof x === "object" && (x as { type: string }).type === "builtin"
   )
 }
 
-export type SExpr = Nil | Num | Bool | Sym | Cons
+export type UserFunc = {
+  type: "lambda"
+  name: string
+  arity: Arity
+  requiredParams: string[]
+  optionalParams: { name: string; defaultVal: SExpr }[]
+  restParam: string | undefined
+  body: SExpr
+}
+export function makeUserFunc(
+  name: string,
+  requiredParams: string[],
+  optionalParams: { name: string; defaultVal: SExpr }[],
+  restParam: string | undefined,
+  body: SExpr
+): UserFunc {
+  return {
+    type: "lambda",
+    name,
+    arity: {
+      required: requiredParams.length,
+      optional: optionalParams.length,
+      hasRest: restParam !== undefined,
+    },
+    requiredParams,
+    optionalParams,
+    restParam,
+    body,
+  }
+}
+export function isUserFunc(x: SExpr): x is UserFunc {
+  return x && typeof x === "object" && (x as { type: string }).type === "lambda"
+}
+
+export type Func = BuiltinFunc | UserFunc
+export function isFunc(x: any): x is Func {
+  return isBuiltinFunc(x) || isUserFunc(x)
+}
+
+export type Proc = SpForm | Func
+export function isProc(x: any): x is Proc {
+  return isSpForm(x) || isFunc(x)
+}
+
+export type SExpr = Nil | Num | Bool | Sym | Cons | Proc
 
 export function toBool(x: SExpr): Bool {
   return makeBool(!isNil(x) && x !== FALSE)

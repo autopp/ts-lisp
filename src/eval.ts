a35144e33dbca619196f17042777e6f03a8082fa
autopp <autopp.inc@gmail.com>
2020-12-06 00:36:42 +0900

    feat: eval symbol

--- src/eval.ts
+++ src/eval.ts
@@ -1,12 +1,17 @@
 import { Env } from "./env"
-import { Ok, Result } from "./result"
-import { isBool, isNil, isNum, SExpr } from "./sexpr"
+import { Err, Ok, Result } from "./result"
+import { isBool, isNil, isNum, isSym, SExpr } from "./sexpr"
 
 export type EvalResult = Result<SExpr, string>
 
 export function evalSExpr(sexpr: SExpr, env: Env): EvalResult {
   if (isNil(sexpr) || isBool(sexpr) || isNum(sexpr)) {
     return new Ok(sexpr)
+  } else if (isSym(sexpr)) {
+    const value = env.lookup(sexpr)
+    return value !== undefined
+      ? new Ok(value)
+      : new Err(`${sexpr} is not defined`)
   } else {
     throw new Error("not implemented")
   }
--- test/eval.test.ts
+++ test/eval.test.ts
@@ -1,6 +1,6 @@
 import { Env } from "@/env"
 import { EvalResult, evalSExpr } from "@/eval"
-import { Ok } from "@/result"
+import { Err, Ok } from "@/result"
 import { NIL, TRUE, FALSE, makeNum, makeSym, makeCons, SExpr } from "@/sexpr"
 import { describeEach } from "./helper"
 
@@ -11,9 +11,21 @@ describe("evalSExpr", () => {
       ["with #t", TRUE, "#t", new Ok(TRUE)],
       ["with #f", FALSE, "#f", new Ok(FALSE)],
       ["with 42", makeNum(42), "42", new Ok(makeNum(42))],
+      [
+        "with answer (defined in env)",
+        makeSym("answer"),
+        "42",
+        new Ok(makeNum(42)),
+      ],
+      [
+        "with unknown (not defined in env)",
+        makeSym("unknown"),
+        "error",
+        new Err("unknown is not defined"),
+      ],
     ],
     (sexpr, expectedString, expected) => {
-      const env = new Env([], null)
+      const env = new Env([["answer", makeNum(42)]], null)
 
       it(`returns ${expectedString}`, () => {
         expect(evalSExpr(sexpr, env)).toEqual(expected)

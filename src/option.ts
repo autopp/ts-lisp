d4861cfa9515c0afa0f8af3dc2bee88581f8a191
autopp <autopp.inc@gmail.com>
2020-12-06 13:12:11 +0900

    feat: add Option.isDefined()

--- src/option.ts
+++ src/option.ts
@@ -2,12 +2,17 @@ import { Result, Ok, Err } from "./result"
 
 export type Option<T> = Result<T, void> & OptionInterface<T>
 interface OptionInterface<T> {
+  isDefined(this: Option<T>): this is Some<T>
   okOr<E>(e: E): Result<T, E>
 }
 
 export class Some<T> extends Ok<T, void> implements OptionInterface<T> {
   toString(): string {
     return `Some(${this.value})`
+  }
+
+  isDefined(this: Option<T>): this is Some<T> {
+    return this.isOk()
   }
 
   okOr<E>(_e: E): Result<T, E> {
@@ -18,6 +23,10 @@ export class Some<T> extends Ok<T, void> implements OptionInterface<T> {
 export class None<T> extends Err<T, void> implements OptionInterface<T> {
   toString(): string {
     return "None"
+  }
+
+  isDefined(this: Option<T>): this is Some<T> {
+    return this.isOk()
   }
 
   okOr<E>(e: E): Result<T, E> {
--- test/option.test.ts
+++ test/option.test.ts
@@ -1,11 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Option, Some, None } from "@/option"
 import { Err, Ok, Result } from "@/result"
 import { describeEach } from "./helper"
 
+type TestOption = Option<number>
+
+const someVal: TestOption = new Some(42)
+const noneVal: TestOption = new None()
+
+function describeOptionMethod<T extends any[]>(
+  name: string,
+  someCase: [...T],
+  noneCase: [...T],
+  fn: (option: TestOption, ...args: T) => any
+): void
+
+function describeOptionMethod(
+  name: string,
+  someFn: (option: TestOption) => any,
+  noneFn: (option: TestOption) => any
+): void
+
+function describeOptionMethod<T extends any[]>(
+  name: string,
+  ...args:
+    | [
+        someCase: [...T],
+        noneCase: [...T],
+        f: (option: TestOption, ...args: T) => any
+      ]
+    | [someF: (option: TestOption) => any, someF: (option: TestOption) => any]
+): void {
+  describe(name, () => {
+    if (args.length === 3) {
+      const [someCase, noneCase, f] = args
+      describeEach(
+        "on %s",
+        [
+          [someVal, ...someCase],
+          [noneVal, ...noneCase],
+        ],
+        f
+      )
+    } else {
+      const [someF, noneF] = args
+
+      describe(`on ${someVal}`, () => someF(someVal))
+      describe(`on ${noneVal}`, () => noneF(noneVal))
+    }
+  })
+}
+
 describe("Option", () => {
   describe(".okOr", () => {
     describeEach<[Option<number>, Result<number, string>]>(
-      "on %s",
+      'on %s with "error"',
       [
         [new Some(42), new Ok(42)],
         [new None(), new Err("error")],
@@ -16,5 +65,11 @@ describe("Option", () => {
         })
       }
     )
+  })
+
+  describeOptionMethod(".isDefined()", [true], [false], (option, expected) => {
+    it(`returns ${expected}`, () => {
+      expect(option.isDefined()).toEqual(expected)
+    })
   })
 })

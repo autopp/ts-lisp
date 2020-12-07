450ebcc4a693d5a52de80d1281965a24efe64403
autopp <autopp.inc@gmail.com>
2020-12-07 23:50:35 +0900

    feat: impl eval bulitin func

--- src/eval.ts
+++ src/eval.ts
@@ -1,6 +1,21 @@
 import { Env } from "./env"
 import { Err, Ok, Result } from "./result"
-import { isBool, isNil, isNum, isSym, SExpr } from "./sexpr"
+import {
+  Arity,
+  Func,
+  isBool,
+  isBuiltinFunc,
+  isCons,
+  isFunc,
+  isNil,
+  isNum,
+  isProc,
+  isSpForm,
+  isSym,
+  SExpr,
+  SpForm,
+  toArray,
+} from "./sexpr"
 
 export type EvalResult = Result<SExpr, string>
 
@@ -13,6 +28,61 @@ export function evalSExpr(sexpr: SExpr, env: Env): EvalResult {
       ? new Ok(value)
       : new Err(`${sexpr} is not defined`)
   } else {
-    throw new Error("not implemented")
+    return toArray(sexpr)
+      .filter(({ extra }) => !extra.isDefined())
+      .okOr("expected list format")
+      .flatMap(({ list }) => {
+        const [shouldProc, ...args] = list
+        return evalSExpr(shouldProc, env).flatMap((proc) => {
+          if (isFunc(proc)) {
+            return callFunc(proc, args, env)
+          }
+
+          return new Err("expected proc, but got not proc")
+        })
+      })
+  }
+}
+
+function callFunc(func: Func, args: SExpr[], env: Env): EvalResult {
+  return args
+    .reduce<Result<SExpr[], string>>(
+      (result, arg) =>
+        result.flatMap((list) =>
+          evalSExpr(arg, env).map((evaledArg) => {
+            list.push(evaledArg)
+            return list
+          })
+        ),
+      new Ok(new Array<SExpr>())
+    )
+    .flatMap((evaledArgs) => validateByArity(evaledArgs, func.arity))
+    .flatMap((evaledArgs) => {
+      if (isBuiltinFunc(func)) {
+        return func.body(evaledArgs, env)
+      } else {
+        throw new Error("not implemented")
+      }
+    })
+}
+
+function validateByArity(list: SExpr[], arity: Arity): Result<SExpr[], string> {
+  const length = list.length
+
+  if (arity.hasRest) {
+    return length >= arity.required
+      ? new Ok(list)
+      : new Err(`expected ${arity.required} or more than, but got ${length}`)
+  } else {
+    if (arity.optional > 0) {
+      const max = arity.required + arity.optional
+      return length >= arity.required && length <= max
+        ? new Ok(list)
+        : new Err(`expected ${arity.required}...${max}, but got ${length}`)
+    } else {
+      return length === arity.required
+        ? new Ok(list)
+        : new Err(`expected ${arity.required}, but got ${length}`)
+    }
   }
 }
--- test/eval.test.ts
+++ test/eval.test.ts
@@ -1,7 +1,17 @@
 import { Env } from "@/env"
 import { EvalResult, evalSExpr } from "@/eval"
 import { Err, Ok } from "@/result"
-import { NIL, TRUE, FALSE, makeNum, makeSym, makeCons, SExpr } from "@/sexpr"
+import {
+  NIL,
+  TRUE,
+  FALSE,
+  makeNum,
+  makeSym,
+  SExpr,
+  makeBuiltinFunc,
+  isNum,
+  makeList,
+} from "@/sexpr"
 import { describeEach } from "./helper"
 
 describe("evalSExpr", () => {
@@ -23,9 +33,45 @@ describe("evalSExpr", () => {
         "error",
         new Err("unknown is not defined"),
       ],
+      [
+        "with (answer 41)",
+        makeList(makeSym("answer"), makeNum(41)),
+        "error",
+        new Err("expected proc, but got not proc"),
+      ],
+      [
+        "with (inc 41)",
+        makeList(makeSym("inc"), makeNum(41)),
+        "42",
+        new Ok(makeNum(42)),
+      ],
+      [
+        "with (inc)",
+        makeList(makeSym("inc")),
+        "42",
+        new Err("expected 1, but got 0"),
+      ],
+      [
+        "with (inc 41 1)",
+        makeList(makeSym("inc"), makeNum(41), makeNum(1)),
+        "42",
+        new Err("expected 1, but got 2"),
+      ],
     ],
     (sexpr, expectedString, expected) => {
-      const env = new Env([["answer", makeNum(42)]], null)
+      const inc = makeBuiltinFunc("inc", { required: 1 }, ([sexpr]) => {
+        if (isNum(sexpr)) {
+          return new Ok(makeNum(sexpr + 1))
+        }
+        return new Err("is not number")
+      })
+      const env = new Env(
+        [
+          ["inc", inc],
+          ["answer", makeNum(42)],
+        ],
+        null
+      )
 
       it(`returns ${expectedString}`, () => {
         expect(evalSExpr(sexpr, env)).toEqual(expected)
